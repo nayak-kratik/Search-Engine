29f5dd08d41583cfe7826405d2465be7bb2802e4
Kratik Nayak <kratiknayak.7@gmail.com>
2020-06-13 22:45:34 +0530

    fixed minor UI glitch in book search result section

--- src/views/AutoComplete.jsx
+++ src/views/AutoComplete.jsx
@@ -5,23 +5,25 @@ import SearchEngine from "../utils/search";
 const AutoComplete = () => {
   const [searchInstance] = useState(new SearchEngine());
   const [searchResult, setSearchResult] = useState([]);
-  const [selectedBook ,setSelectedBook] = useState(undefined)
-  const [allSelectedBooks,setAllSelectedBook] = useState([])
+  const [selectedBook, setSelectedBook] = useState(undefined);
+  const [allSelectedBooks, setAllSelectedBook] = useState([]);
 
+  const populateInputField = (title) => {
+    document.getElementsByClassName("search-bar__input")[0].value = title;
+    document.getElementsByClassName("search-bar__input")[0].style.color =
+      "var(--highlight-color)";
+  };
+  const unPopulateInputField = () => {
+    document.getElementsByClassName("search-bar__input")[0].value = "";
+    document.getElementsByClassName("search-bar__input")[0].style.color =
+      "var(--text-color)";
+    setSelectedBook(undefined);
+  };
 
-  const populateInputField =(title) =>{
-    document.getElementsByClassName('search-bar__input')[0].value = title
-    document.getElementsByClassName('search-bar__input')[0].style.color = 'var(--highlight-color)'
-  }
-  const unPopulateInputField =() =>{
-    document.getElementsByClassName('search-bar__input')[0].value = ''
-    document.getElementsByClassName('search-bar__input')[0].style.color = 'var(--text-color)'
-  }
-
-  const highlightSelectedBook = (chosenBook) =>{
-    setSearchResult([])
-    populateInputField(chosenBook.title)
-   }
+  const highlightSelectedBook = (chosenBook) => {
+    setSearchResult([]);
+    populateInputField(chosenBook.title);
+  };
 
   const onChange = (event) => {
     const searchQuery = event.target.value.toLowerCase();
@@ -33,19 +35,20 @@ const AutoComplete = () => {
     );
   };
 
-  const onSelect =(e) =>{
+  const onSelect = (e) => {
     // Convert DOMStringMap to Object
     // The DOMStringMap interface is used for the HTMLElement.dataset attribute,to represent data for custom attributes added to elements.
-    const chosenBook = Object.assign({}, e.target.dataset)
-    setSelectedBook(
-      chosenBook
-    )
-    highlightSelectedBook(chosenBook)
-  }
-  
+    const chosenBook = Object.assign({}, e.target.dataset);
+    setSelectedBook(chosenBook);
+    highlightSelectedBook(chosenBook);
+  };
+
   const onSubmit = (e) => {
-    setAllSelectedBook(prevSelectedBooks => [...prevSelectedBooks,selectedBook]);
-    unPopulateInputField()
+    setAllSelectedBook((prevSelectedBooks) => [
+      ...prevSelectedBooks,
+      selectedBook,
+    ]);
+    unPopulateInputField();
   };
 
   const searchBarParams = {
@@ -53,12 +56,12 @@ const AutoComplete = () => {
     onSubmit,
     onSelect,
     searchResult,
-    selectedBook
+    selectedBook,
   };
   return (
     <>
       <SearchBar {...searchBarParams}></SearchBar>
-      <SearchResult allBooks={allSelectedBooks} ></SearchResult>
+      <SearchResult allBooks={allSelectedBooks}></SearchResult>
     </>
   );
 };
--- src/views/components/DisplayCard.jsx
+++ src/views/components/DisplayCard.jsx
@@ -4,13 +4,13 @@ import "../../App.css";
 
 const DisplayCard = ({ title, summary, author }) => {
   return (
-    <div className="flip-container box-shadow-outset search-result__display-card m-3">
+    <div className="flip-container box-shadow-outset search-result__display-card m-3 flex-1">
       <div className="flipper h-100 w-100 position-relative">
-        <div className="front position-absolute  h-100  px-3 d-flex flex-column  justify-content-center ">
-          <h2>{title}</h2>
-          <sup>By - {author}</sup>
+        <div className="front position-absolute  h-100  w-100 d-flex flex-column  justify-content-center ">
+          <h2 className="px-3">{title}</h2>
+          <span>By - {author}</span>
           <span className="read-more position-absolute">
-            Hover to read summary
+            Hover to read the summary
           </span>
         </div>
         <div className="back position-absolute h-100  px-3  d-flex overflow-scroll ">
